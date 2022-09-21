664631101a0a232d1ec5d01ab33f161cf8351f56
siva <sivashakthiarun.c@gmail.com>
2022-09-21 10:36:04 +0530

    repush

---
+++ main.js
@@ -0,0 +1,21 @@
+/*
+var i;
+for (i = 1; i <= 20; i++) {
+
+    if (i % 2 != 0) {
+        console.log(i);
+    }
+}
+*/
+
+var i=0;
+var a=20;
+while(i<=a){
+   if (i % 2 != 0) {
+        console.log(i);
+    } 
+    i++;
+}
+
+
+
